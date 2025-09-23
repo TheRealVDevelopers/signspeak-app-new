e0061a476ecf1cf8b2ff49b3b2eeb545a39106c8
run fast <runfast060425@gmail.com>
2025-09-23 21:37:01 +0000

    that N in the corner need to be removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ export default function HomePage() {
         <Separator />
         <FeaturesSection />
         <Separator />
-        <IslEducationSection />
         <Separator />
         <TechnologySection />
         <Separator />
--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -22,7 +22,7 @@ export function HeroSection() {
               <Link href="/detect">Start Detecting</Link>
             </Button>
             <Button asChild variant="outline" size="lg">
-              <Link href="#features">Learn More</Link>
+              <Link href="">Learn More</Link>
             </Button>
           </div>
         </div>
